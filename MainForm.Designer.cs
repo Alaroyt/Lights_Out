bd492dbf0d34ee7b7b29dfaa7752bab9055bdb9e
Alaroyt <54007417+Alaroyt@users.noreply.github.com>
2020-02-10 22:03:45 +0300

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: User
+ * Дата: 10.02.2020
+ * Время: 16:10
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+namespace Lights_Out
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.button1.Location = new System.Drawing.Point(80, 115);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(158, 68);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Начать игру";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(80, 51);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 29);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(43, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(329, 45);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Размер игрового поля NxN";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(43, 54);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "N=";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(384, 262);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.KeyPreview = true;
+			this.Name = "MainForm";
+			this.Text = "Lights_Out";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,151 @@
+﻿//
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lights_Out
+{
+	public partial class MainForm : Form
+	{
+		int count, HowPictureBox;
+		Dictionary <int,Point> ExistingBox;
+		Dictionary <int,int> HachCode_OfBox;
+		int _Widht = 600, _Height = 600, _SizeOfSquare = 60;
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			
+		}
+
+		void _genmap(int N)
+		{
+			Controls.Clear();
+			
+			_Widht = _SizeOfSquare * N;
+			_Height = _SizeOfSquare * N;
+			
+			Height = (int)(_Height * 1.3);
+			Width = (int)(_Widht * 1.2);
+
+			
+			
+			
+			for (int i = 0; i <= _Height / _SizeOfSquare; i++) {
+				PictureBox field = new PictureBox();
+				field.Size = new Size(_Widht, 1);
+				field.Location = new Point(0, _SizeOfSquare * i);
+				field.BackColor = Color.Gray;
+				Controls.Add(field);
+			}
+			
+			for (int i = 0; i <= _Widht / _SizeOfSquare; i++) {
+				PictureBox field = new PictureBox();
+				field.Size = new Size(1, _Height);
+				field.Location = new Point(_SizeOfSquare * i, 0);
+				field.BackColor = Color.Gray;
+				Controls.Add(field);
+			}
+			
+			
+			
+			count = Controls.Count;
+			HowPictureBox = N * N;
+			
+			HachCode_OfBox = new Dictionary<int,int>();
+			ExistingBox = new Dictionary<int, Point>();
+			var ExistingBox2 = new Dictionary<int, Point>();
+			for (int p = 1, i = 0; i < N; i++) {
+				for (int j = 0; j < N; p++, j++) {
+					
+					PictureBox field = new PictureBox();
+					
+					field.Size = new Size(_SizeOfSquare, _SizeOfSquare);
+					field.Location = new Point(i * _SizeOfSquare, j * _SizeOfSquare);
+					field.BackColor = Color.Lime;
+					field.Click += (MyClick);
+					Controls.Add(field);
+					
+					ExistingBox.Add(Controls.Count - 1, new Point(i * _SizeOfSquare, j * _SizeOfSquare));
+					HachCode_OfBox.Add(field.GetHashCode(), Controls.Count - 1);
+					
+				}
+			}
+		}
+
+		int[] ExixstsPoints(Point temp)
+		{
+			Point[] pointsNearPictureBox = {
+				new Point(temp.X, temp.Y),
+				new Point(temp.X + _SizeOfSquare, temp.Y),
+				new Point(temp.X - _SizeOfSquare, temp.Y),
+				new Point(temp.X, temp.Y + _SizeOfSquare),
+				new Point(temp.X, temp.Y - _SizeOfSquare),
+			};
+			
+			List<Point> temp2 = new List<Point>();
+			foreach (var el in ExistingBox) {
+				foreach (var el2 in pointsNearPictureBox) {
+					if (el.Value == el2)
+						temp2.Add(el2);
+				}
+			}
+			List<int> index = new List<int>();
+			foreach (var el in ExistingBox) {
+				foreach (var el2 in pointsNearPictureBox) {
+					if (el.Value == el2) {
+						index.Add(el.Key);
+					}
+				}
+			}
+			return index.ToArray();
+			
+		}
+
+		int _WhichPictureWasClicked(int i)
+		{
+			foreach (var el in HachCode_OfBox) {
+				if (i == el.Key)
+					return el.Value;
+			}
+			return 0;
+		}
+		void MyClick(object sender, EventArgs e)
+		{
+			
+			Point temp = new Point(0, 0);
+			
+			int index = _WhichPictureWasClicked(sender.GetHashCode());
+			
+			foreach (var el in ExistingBox) {
+				if (index == el.Key)
+					temp = el.Value;
+			}
+			
+			int[] IndexSquare = ExixstsPoints(temp);
+			
+			foreach (var el in IndexSquare) {
+				if (Controls[el].BackColor == Color.Lime) {
+					Controls[el].BackColor = Color.Azure;
+				} else {
+					Controls[el].BackColor = Color.Lime;
+				}
+			}
+		}
+
+		void Button1Click(object sender, EventArgs e)
+		{
+			if (int.Parse(textBox1.Text) < 10) {
+				_genmap(int.Parse(textBox1.Text));
+			} else
+				MessageBox.Show("N должно быть меньше 10");
+		}
+		void MainFormKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape) {
+				Close();
+			}
+		}
+	}
+}
